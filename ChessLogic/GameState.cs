241738f36a9a5fb038308a17f1b28cc1077c673c
Emil <Emil@MYDESKTOP>
2023-12-03 15:41:24 +0200

    50 Move Rule added

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -11,6 +11,9 @@ namespace ChessLogic
         public Board Board { get; }
         public Player CurrentPlayer { get; private set; }
         public Result Result { get; private set; } = null;
+
+        private int noCaptureOrPawnMoves = 0;
+
         public GameState(Player player, Board board)
         {
             CurrentPlayer = player;
@@ -32,7 +35,17 @@ namespace ChessLogic
         public void MakeMove(Move move)
         {
             Board.SetPawnSkipPosition(CurrentPlayer, null);
-            move.Execute(Board);
+            bool captureOrPawn = move.Execute(Board);
+
+            if(captureOrPawn)
+            {
+                noCaptureOrPawnMoves = 0;
+            }
+            else
+            {
+                noCaptureOrPawnMoves++;
+            }
+
             CurrentPlayer = CurrentPlayer.Opponent();
             CheckForGameOver();
         }
@@ -64,10 +77,20 @@ namespace ChessLogic
             {
                 Result = Result.Draw(EndReason.InsufficientMaterial);
             }
+            else if(FiftyMoveRule())
+            {
+                Result = Result.Draw(EndReason.FiftyMoveRule);
+            }
         }
         public bool IsGameOver()
         {
             return Result != null;
+        }
+
+        private bool FiftyMoveRule()
+        {
+            int fullMoves = noCaptureOrPawnMoves / 2;
+            return fullMoves == 50;
         }
     }
 }
--- ChessLogic/Moves/PawnPromotion.cs
+++ ChessLogic/Moves/PawnPromotion.cs
@@ -34,7 +34,7 @@ namespace ChessLogic
             };
         }
 
-        public override void Execute(Board board)
+        public override bool Execute(Board board)
         {
             Piece pawn = board[FromPos];
             board[FromPos] = null;
@@ -42,6 +42,8 @@ namespace ChessLogic
             Piece promotionPiece = CreatePromotionPiece(pawn.Color);
             promotionPiece.HasMoved = true;
             board[ToPos] = promotionPiece;
+
+            return true;
         }
     }
 }
